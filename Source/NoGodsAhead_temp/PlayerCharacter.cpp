6d24902b0e69114eee662912e8e2e46f879f6f8c
DZIKU <sebed1234@gmail.com>
2026-09-03 20:07:28 +0200

    Added a simple forward / backward / left / right movement system

---
+++ Source/NoGodsAhead_temp/PlayerCharacter.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerCharacter.h"
+
+// Sets default values
+APlayerCharacter::APlayerCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// Rejestrujemy ruch z klawiatury
+	PlayerInputComponent->BindAxis("MoveForward", this, &APlayerCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &APlayerCharacter::MoveRight);
+
+	// Rejestrujemy myszke
+	PlayerInputComponent->BindAxis("Turn", this, &APlayerCharacter::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APlayerCharacter::AddControllerPitchInput);
+}
+
+// Logika ruchu przod / tyl
+void APlayerCharacter::MoveForward(float Value) {
+	AddMovementInput(GetActorForwardVector(), Value);
+}
+
+// Logika ruchu prawo / lewo
+void APlayerCharacter::MoveRight(float Value) {
+	AddMovementInput(GetActorRightVector(), Value);
+}
